038119c2f699c807d49efc567f9d4a97d530e909
ik2m <39111330+ik2m@users.noreply.github.com>
2026-02-18 01:46:35 +0900

    トップメニュー作成

--- src/components/TopCustom.vue
+++ src/components/TopCustom.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const menuItems = [
+  { name: "3DCG", link: "/archive/category/3DCG" },
+  { name: "プログラミング", link: "/archive/category/プログラミング" },
+  { name: "デザイン", link: "/archive/category/デザイン" },
+  { name: "日記", link: "/archive/category/日記" },
+];
+</script>
 
 <template>
-  <div>テストメニュー</div>
+  <div class="font-sans font-bold">
+    <nav>
+      <ul class="flex justify-around bg-gray-100">
+        <li v-for="item in menuItems">
+          <a :href="item.link">{{ item.name }}</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <style scoped></style>
