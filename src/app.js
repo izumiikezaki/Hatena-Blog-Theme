6016a1bceeddece1e630dfbe7f6e1db26ed7a1d9
ik2m <39111330+ik2m@users.noreply.github.com>
2026-02-18 01:14:38 +0900

    intall vue

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import { createApp } from "vue";
+import TopCustom from "./components/TopCustom.vue";
+
+createApp(TopCustom).mount("#top-custom");
---
+++ src/components/TopCustom.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>テストメニュー</div>
+</template>
+
+<style scoped></style>
