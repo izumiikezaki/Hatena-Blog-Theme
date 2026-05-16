c92c2bc5aefa4f70f3969b3273efee0ca5acb8c3
ik2m <39111330+ik2m@users.noreply.github.com>
2026-05-16 22:39:29 +0900

    PostCSSプラグイン "add-charset" をコメントアウト

--- vite.config.js
+++ vite.config.js
@@ -21,23 +21,23 @@ export default defineConfig({
     postcss: {
       plugins: [
         autoprefixer(),
-        {
-          postcssPlugin: "add-charset",
-          Once(root) {
-            /** @charset "utf-8"; がビルド時に消えてしまうので追記する**/
-            // すでに @charset がある場合は追加しない
-            const hasCharset = root.nodes.some(
-              (node) => node.type === "atrule" && node.name === "charset",
-            );
-            if (!hasCharset) {
-              root.prepend({
-                name: "charset",
-                params: '"utf-8"',
-                type: "atrule",
-              });
-            }
-          },
-        },
+        // {
+        //   postcssPlugin: "add-charset",
+        //   Once(root) {
+        //     /** @charset "utf-8"; がビルド時に消えてしまうので追記する**/
+        //     // すでに @charset がある場合は追加しない
+        //     const hasCharset = root.nodes.some(
+        //       (node) => node.type === "atrule" && node.name === "charset",
+        //     );
+        //     if (!hasCharset) {
+        //       root.prepend({
+        //         name: "charset",
+        //         params: '"utf-8"',
+        //         type: "atrule",
+        //       });
+        //     }
+        //   },
+        // },
       ],
     },
   },
